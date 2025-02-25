2f476eb2b4fc6ca1f07c1ab75205904a3eab68b7
HanZo HanZo_7 <andriyonyschuk@gmail.com>
2025-02-25 13:49:38 +0200

    Fix format

--- AndrewDAL/Models/ContactForm.cs
+++ AndrewDAL/Models/ContactForm.cs
@@ -14,13 +14,10 @@ namespace AndrewDAL.Models
         public int Id { get; set; }
         public string NameLabel { get; set; } = string.Empty;
         public string NamePlaceholder { get; set; } = string.Empty;
-
         public string EmailLabel { get; set; } = string.Empty;
         public string EmailPlaceholder { get; set; } = string.Empty;
-
         public string TitleLabel { get; set; } = string.Empty;
         public string TitlePlaceholder { get; set; } = string.Empty;
-
         public string MessageLabel { get; set; } = string.Empty;
         public string MessagePlaceholder { get; set; } = string.Empty;
     }
